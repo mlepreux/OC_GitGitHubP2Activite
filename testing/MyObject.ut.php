54fb7ea0858ef07d51a461a0631077fe6f3544e6
maxime <maxime.lepreux@laposte.net>
2017-02-16 23:30:01 +0100

    Tests unitaires pour la classe métier MyObject

---
+++ testing/MyObject.ut.php
@@ -0,0 +1,18 @@
+/* tests unitaires de la classe métier MyObject */
+
+Class MyObjectUT
+{
+//cas d'utilisation 1 : blabla bla
+function UC_001()
+{
+...
+}
+
+//cas d'utilisation 2: blabla bla
+function UC_002()
+{
+...
+}
+
+....
+}
